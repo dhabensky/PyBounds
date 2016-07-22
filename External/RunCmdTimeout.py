71f99047ce7ea4a61d77ff6a950bae20c35382e9
maxim.mishechkin <mish747@gmail.com>
2016-07-22 16:06:10 +0300

    Added new mode smart in PyBounds and Compression Parametrs

--- External/RunCmdTimeout.py
+++ External/RunCmdTimeout.py
@@ -8,87 +8,87 @@ import subprocess
 
 
 def debug_print(s, name_log):
-	directory = "/home/max/TestFile/"
-	directory += name_log
-	format_file = ".txt"
-	directory += format_file
+    directory = "/home/max/TestFile/"
+    directory += name_log
+    format_file = ".txt"
+    directory += format_file
 
-	ffff = open(directory, 'a')
-	ffff.write(s)
-	ffff.write('\n')
-	ffff.close()
+    ffff = open(directory, 'a')
+    ffff.write(s)
+    ffff.write('\n')
+    ffff.close()
 
 def check_num(s):
-	s_res = ''	
-	for i in s:
-		if(i >= '0' and i <= '9' or i == ' '):
-			s_res += i	
-	return s_res
+    s_res = ''
+    for i in s:
+        if(i >= '0' and i <= '9' or i == ' '):
+            s_res += i
+    return s_res
 
 class RunCmd(threading.Thread):
 
-	def __init__(self, cmd, progname):
-		super(RunCmd, self).__init__()
-		self.cmd = cmd
-		self.progname = progname
-		try:
-			self.existing_pids = subprocess.check_output(["pidof", self.progname]).split()
-		except:
-			self.existing_pids = []
-	pass
+    def __init__(self, cmd, progname):
+        super(RunCmd, self).__init__()
+        self.cmd = cmd
+        self.progname = progname
+        try:
+            self.existing_pids = subprocess.check_output(["pidof", self.progname]).split()
+        except:
+            self.existing_pids = []
+    pass
 
-	def run(self):
-		# print(self.cmd)
-		os.system(self.cmd)
-	pass
+    def run(self):
+        # print(self.cmd)
+        os.system(self.cmd)
+    pass
 
-	def stop(self):
-		try:
+    def stop(self):
+        try:
 
-			try:
-				pids = subprocess.check_output(["pidof", self.progname]).split()
-			except:
-				pids = []
+            try:
+                pids = subprocess.check_output(["pidof", self.progname]).split()
+            except:
+                pids = []
 
-			for pid in self.existing_pids:
-				try:
-					pids.remove(pid)
-				except:
-					pass
+            for pid in self.existing_pids:
+                try:
+                    pids.remove(pid)
+                except:
+                    pass
 
-			# print(pids)
-			if len(pids) != 0:	
-				pids_str = ' '.join(str(i) for i in pids)
-				pids_str = check_num(pids_str)
-				# print(pids_str)			
-				os.system("kill -9 " + pids_str + " 2>/dev/null")
-		except Exception as ex:			
-			print(str(ex))
-		pass
-	pass
+            # print(pids)
+            if len(pids) != 0:
+                pids_str = ' '.join(str(i) for i in pids)
+                pids_str = check_num(pids_str)
+                # print(pids_str)
+                os.system("kill -9 " + pids_str + " 2>/dev/null")
+        except Exception as ex:
+            print(str(ex))
+        pass
+    pass
 
 pass  # RunCmd
 
 
 class RunCmdTimeout:
 
-	def __init__(self, cmd, progname, seconds):
-		self.runcmd = RunCmd(cmd, progname)
-		self.timeout = seconds
-	pass
+    def __init__(self, cmd, progname, seconds):
+        self.runcmd = RunCmd(cmd, progname)
+        self.timeout = seconds
+    pass
 
-	def run(self):
-		self.runcmd.start()
-		start = datetime.datetime.now()
-		delta = datetime.timedelta(0, self.timeout, 0)
+    def run(self):
+        self.runcmd.start()
+        start = datetime.datetime.now()
+        delta = datetime.timedelta(0, self.timeout, 0)
 
-		while self.runcmd.is_alive() and datetime.datetime.now() - start < delta:
-			time.sleep(0.1)
+        while self.runcmd.is_alive() and datetime.datetime.now() - start < delta:
+            time.sleep(0.1)
 
-		if self.runcmd.is_alive():
-			self.runcmd.stop()
-			return True
-		else:
-			return False
+        if self.runcmd.is_alive():
+            self.runcmd.stop()
+            return True
+        else:
+            return False
 
 pass  # RunCmdTimeout
--- PyBounds.py
+++ PyBounds.py
@@ -12,26 +12,26 @@ from Shared.Files import *
 
 
 def usage():
-	print()
-	print("USAGE:")
-	print()
-	print("  -h | --help                          - prints this message")
-	print()
-	print(" [-v | -s] [-t secs] [-m kilobytes] task_file")
-	print("                                       - run task from 'task_file'")
-	print("                                          in normal, (v)erbose or (s)ilent mode")
-	print("                                          with optional time and/or memory limit")
-	print()
-	print("  -r | --reg module cls1 [ cls2 [...]] - register argument classes 'cls1'")
-	print("                                          'cls2', ... from module 'module'")
-	print()
-	print("  -u | --unreg cls1 [ cls2 [...] ]     - unregister argument classes 'cls1'")
-	print("                                          'cls2', ... ")
-	print()
-	print("  -p | --print                         - show registered classes")
-	print()
-	print("  -c | --clear                         - clear registered classes")
-	print()
+    print()
+    print("USAGE:")
+    print()
+    print("  -h | --help                          - prints this message")
+    print()
+    print(" [-v | -s] [-t secs] [-m kilobytes] task_file")
+    print("                                       - run task from 'task_file'")
+    print("                                          in normal, (v)erbose or (s)ilent mode")
+    print("                                          with optional time and/or memory limit")
+    print()
+    print("  -r | --reg module cls1 [ cls2 [...]] - register argument classes 'cls1'")
+    print("                                          'cls2', ... from module 'module'")
+    print()
+    print("  -u | --unreg cls1 [ cls2 [...] ]     - unregister argument classes 'cls1'")
+    print("                                          'cls2', ... ")
+    print()
+    print("  -p | --print                         - show registered classes")
+    print()
+    print("  -c | --clear                         - clear registered classes")
+    print()
 pass
 
 verbose = False
@@ -39,129 +39,135 @@ silent = False
 
 def main():
 
-	global verbose
-	global silent
+    global verbose
+    global silent
+    smart = False
+    arch = "x86"
 
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "vst:m:h:r:upc", ["help", "register=", "unregister", "print", "clear"])
-	except getopt.GetoptError as err:
-		print("    " + str(err))
-		usage()
-		sys.exit(2)
-	pass
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "vst:m:h:r:upc", ["help", "register=", "unregister", "print", "clear", "smart="])
+    except getopt.GetoptError as err:
+        print("    " + str(err))
+        usage()
+        sys.exit(2)
+    pass
 
-	# if not os.path.exists(os.path.expanduser("~/.PyBounds")):
-	# 	os.mkdir(os.path.expanduser("~/.PyBounds"))
+    # if not os.path.exists(os.path.expanduser("~/.PyBounds")):
+    # 	os.mkdir(os.path.expanduser("~/.PyBounds"))
 
-	dir_temp= mkdtemp()
+    dir_temp= mkdtemp()
 
-	files = Files(dir_temp, '')
-	if not os.path.exists(files.regclasses):
-		try:
-			open(files.regclasses, 'w').close()
-		except:
-			pass
+    files = Files(dir_temp, '')
+    if not os.path.exists(files.regclasses):
+        try:
+            open(files.regclasses, 'w').close()
+        except:
+            pass
 
-	if len(sys.argv) == 1:
-		usage()
-		sys.exit()
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit()
 
-	time = 1.5        # sec
-	memory = 1000000  # Kb
+    time = 1.5        # sec
+    memory = 1000000  # Kb
 
-	for o, a in opts:
+    for o, a in opts:
 
-		if o in ("-h", "--help"):
-			usage()
-			sys.exit()
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit()
 
-		elif o == "-v":
-			if silent:
-				print("    silent and verbose are exclusive options")
-				usage()
-				sys.exit(1)
-			verbose = True
+        elif o == "--smart":
+            arch = a
+            smart = True
 
-		elif o == "-s":
-			if verbose:
-				print("    silent and verbose are exclusive options")
-				usage()
-				sys.exit(1)
-			silent = True
+        elif o == "-v":
+            if silent:
+                print("    silent and verbose are exclusive options")
+                usage()
+                sys.exit(1)
+            verbose = True
 
-		elif o == "-t":
-			try:
-				time = float(a)
-			except:
-				print("    time must be a number")
-				usage()
-				exit(1)
+        elif o == "-s":
+            if verbose:
+                print("    silent and verbose are exclusive options")
+                usage()
+                sys.exit(1)
+            silent = True
 
-			if time < 0:
-				print("    time must be positive")
-				usage()
-				exit(1)
+        elif o == "-t":
+            try:
+                time = float(a)
+            except:
+                print("    time must be a number")
+                usage()
+                exit(1)
 
-			if time < 1.5:
-				print("    recommended time limit: > 1.5 seconds")
+            if time < 0:
+                print("    time must be positive")
+                usage()
+                exit(1)
 
-		elif o == "-m":
-			try:
-				memory = int(a)
-			except:
-				print("    memory must be a number")
-				usage()
-				exit(1)
+            if time < 1.5:
+                print("    recommended time limit: > 1.5 seconds")
 
-			if memory < 0:
-				print("    memory must be positive")
-				usage()
-				exit(1)
+        elif o == "-m":
+            try:
+                memory = int(a)
+            except:
+                print("    memory must be a number")
+                usage()
+                exit(1)
 
-			if memory < 100000:
-				print("    recommended memory limit: > 100000 Kb")
+            if memory < 0:
+                print("    memory must be positive")
+                usage()
+                exit(1)
 
-		elif o in ("-r", "--register"):
-			module = a
-			classes = args
-			parser = TaskParser()
-			parser.load_registered_classes(files.regclasses, silent=True)
-			#print "registering classes " + " ".join(classes) + " in module " + module
-			parser.register_by_query(module + " " + " ".join(classes), silent=False)
-			parser.save_registered_classes(files.regclasses)
-			sys.exit()
+            if memory < 100000:
+                print("    recommended memory limit: > 100000 Kb")
 
-		elif o in ("-u", "--unregister"):
-			classes = args
-			parser = TaskParser()
-			parser.load_registered_classes(files.regclasses, silent=True)
-			#print "unregistering classes" + " ".join(classes) + " in module " + module
-			parser.unregister_by_query(" ".join(classes), silent=False)
-			parser.save_registered_classes(files.regclasses)
-			sys.exit()
+        elif o in ("-r", "--register"):
+            module = a
+            classes = args
+            parser = TaskParser()
+            parser.load_registered_classes(files.regclasses, silent=True)
+            #print "registering classes " + " ".join(classes) + " in module " + module
+            parser.register_by_query(module + " " + " ".join(classes), silent=False)
+            parser.save_registered_classes(files.regclasses)
+            sys.exit()
 
-		elif o in ("-p", "--print"):
-			os.system("cat " + files.regclasses + " 2>/dev/null")
-			sys.exit()
+        elif o in ("-u", "--unregister"):
+            classes = args
+            parser = TaskParser()
+            parser.load_registered_classes(files.regclasses, silent=True)
+            #print "unregistering classes" + " ".join(classes) + " in module " + module
+            parser.unregister_by_query(" ".join(classes), silent=False)
+            parser.save_registered_classes(files.regclasses)
+            sys.exit()
 
-		elif o in ("-c", "--clear"):
-			os.system("rm " + files.regclasses + " 2>/dev/null")
-			sys.exit()
+        elif o in ("-p", "--print"):
+            os.system("cat " + files.regclasses + " 2>/dev/null")
+            sys.exit()
 
-		else:
-			pass
+        elif o in ("-c", "--clear"):
+            os.system("rm " + files.regclasses + " 2>/dev/null")
+            sys.exit()
 
-	pass
+        else:
+            pass
 
-	if len(args) > 0:
-		TaskExecutor.main(dir_temp, args[0], time, memory, verbose, silent)
-		rmtree(dir_temp)
-	else:
-		print("    required task_file")
-		usage()
-		exit(1)
+    pass
+
+    if len(args) > 0:
+        TaskExecutor.main(dir_temp, args[0], time, memory, verbose, silent, smart, arch)
+        rmtree(dir_temp)
+    else:
+        print("    required task_file")
+        usage()
+        exit(1)
 
 pass
 
 if __name__ == "__main__":
-	main()
+    main()
